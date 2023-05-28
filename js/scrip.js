7f4d2e55c13086bb03526eb08c016f3fa0df15a6
alishahi <shahy8227@gmail.com>
2023-05-28 10:03:30 -0700

    completing

--- js/scrip.js
+++ js/scrip.js
@@ -1,8 +1,2 @@
-const mainimg = document.querySelector(".main-img");
-const mainhover = document.querySelector(".main-hover");
-console.log(mainimg);
 
-mainimg.addEventListener('mouseenter', function () {
-  mainhover.classList.toggle("main-hover-active");
-});
 
